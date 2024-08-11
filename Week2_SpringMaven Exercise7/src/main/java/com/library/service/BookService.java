5ffb24b2cd30b293c6f4183c0e0f131a132d0632
sayantika04 <160743429+sayantika04@users.noreply.github.com>
2024-08-11 20:43:42 +0530

    Add files via upload

---
+++ Week2_SpringMaven Exercise7/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,15 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter method for BookRepository
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Additional methods for BookService
+}
---
+++ Week2_SpringMaven Exercise7/src/main/java/com/library2/LibraryManagementApplication.java
@@ -0,0 +1,19 @@
+package com.library2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        // Load Spring context
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Perform a test or print out to verify injection
+        System.out.println("BookService bean loaded: " + (bookService != null));
+    }
+}
+
