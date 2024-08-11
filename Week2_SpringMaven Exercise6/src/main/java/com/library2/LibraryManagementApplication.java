470e96d19461f9cc3e1b475be10394715f0a67f4
sayantika04 <160743429+sayantika04@users.noreply.github.com>
2024-08-11 20:42:55 +0530

    Add files via upload

---
+++ Week2_SpringMaven Exercise6/src/main/java/com/library2/LibraryManagementApplication.java
@@ -0,0 +1,15 @@
+package com.library2;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Test the configuration
+        // Example: bookService.someMethod();
+    }
+}
